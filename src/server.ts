1fdc606e2bb495ec2577e5dab1fd42f8ea506b26
Sun <keluar2027@gmail.com>
2026-01-04 00:19:47 +0800

    chore: add log check executablePath

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import puppeteer from 'puppeteer';
 import app from './app';
 import config from './config/config';
 
 app.listen(config.port, () => {
+  console.log("Executable:", puppeteer.executablePath());
   console.log(`Server running on port ${config.port}`);
 });
