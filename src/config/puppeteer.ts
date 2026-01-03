d97eeed0a5f1375030852562fc24bdb881c290ae
Sun <keluar2027@gmail.com>
2026-01-04 01:01:28 +0800

    fix: puppeteer config

--- src/config/puppeteer.ts
+++ src/config/puppeteer.ts
@@ -1,19 +1,9 @@
 import puppeteer, { Browser } from "puppeteer";
-import fs from "fs";
-
-const CHROME_PATH =
-  "/opt/render/.cache/puppeteer/chrome/linux-143.0.7499.169/chrome-linux64/chrome";
 
 export const launchBrowser = async (): Promise<Browser> => {
-  const executablePath = fs.existsSync(CHROME_PATH)
-    ? CHROME_PATH
-    : undefined;
+  const executablePath = puppeteer.executablePath();
 
-  if (executablePath) {
-    console.log("Using Chrome at:", executablePath);
-  } else {
-    console.log("Using Puppeteer default Chrome");
-  }
+  console.log("Using Chrome at:", executablePath);
 
   return puppeteer.launch({
     executablePath,
